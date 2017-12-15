ce1c27cf912e064f5aab68dec105870c410cac65
guozhiqiang <156627977@qq.com>
2017-12-15 17:16:34 +0800

    storm  commit

---
+++ StormDemo/src/main/java/com/gzq/Strom.java
@@ -0,0 +1,10 @@
+package com.gzq;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author guozhiqiang
+ * @created 2017-12-15 17:16.
+ */
+public class Strom {
+}
---
+++ StormDemo/src/test/java/com/gzq/StromTest.java
@@ -0,0 +1,10 @@
+package com.gzq;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author guozhiqiang
+ * @created 2017-12-15 17:15.
+ */
+public class StromTest {
+}
